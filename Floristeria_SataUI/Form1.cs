fd4b3a9bc261cc9d030b5b3bd8fbdedd42144383
Maria Sofia Londono Suarez <lodonosuarezmariasofia@gmail.com>
2025-10-23 00:42:13 -0500

    Agregar archivos de proyecto.

---
+++ Floristeria_SataUI/Form1.cs
@@ -0,0 +1,43 @@
+﻿using Floristeria_SataUI.UserControls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Floristeria_SataUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDash_Click(object sender, EventArgs e)
+        {
+            CargarUserControl(new UCDashboard());
+        }
+
+        private void CargarUserControl(UserControl control)
+        {
+            pnlVentana.Controls.Clear();
+            control.Dock = DockStyle.Fill;
+            pnlVentana.Controls.Add(control);
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Login login = new Login();
+
+            login.Show();
+
+            this.Hide();
+        }
+    }
+}
+
---
+++ Floristeria_SataUI/Login.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Floristeria_SataUI
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnIngreso_Click(object sender, EventArgs e)
+        {
+            Form1 formPrincipal = new Form1();
+            formPrincipal.Show();
+            this.Hide();
+        }
+    }
+}
