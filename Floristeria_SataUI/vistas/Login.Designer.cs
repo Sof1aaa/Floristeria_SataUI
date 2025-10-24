5484fb1e239a37cca0f1dd1a7e4ec1a181cd585c
TeoBelt <matecorrea860@gmail.com>
2025-10-23 21:49:28 -0500

    Add files via upload

---
+++ Floristeria_SataUI/vistas/Login.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace Floristeria_SataUI
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sataEllipseControl1 = new SATAUiFramework.Controls.SATAEllipseControl();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUs = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.btnIngreso = new FrameworkTest.SATAButton();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.cbVer = new System.Windows.Forms.CheckBox();
+            this.btnSalir = new FrameworkTest.SATAButton();
+            this.sataPictureBox3 = new SATAUiFramework.Controls.SATAPictureBox();
+            this.sataPictureBox2 = new SATAUiFramework.Controls.SATAPictureBox();
+            this.sataPictureBox1 = new SATAUiFramework.Controls.SATAPictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.sataPictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sataPictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sataPictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sataEllipseControl1
+            // 
+            this.sataEllipseControl1.CornerRadius = 35;
+            this.sataEllipseControl1.TargetControl = this;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(518, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(240, 30);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Bienvenido/a de nuevo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(533, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Usuario";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(533, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Contraseña";
+            // 
+            // txtUs
+            // 
+            this.txtUs.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUs.Location = new System.Drawing.Point(536, 176);
+            this.txtUs.Name = "txtUs";
+            this.txtUs.Size = new System.Drawing.Size(204, 25);
+            this.txtUs.TabIndex = 8;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPass.Location = new System.Drawing.Point(536, 235);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(204, 25);
+            this.txtPass.TabIndex = 9;
+            // 
+            // btnIngreso
+            // 
+            this.btnIngreso.ButtonText = "Entrar";
+            this.btnIngreso.CheckedBackground = System.Drawing.Color.DodgerBlue;
+            this.btnIngreso.CheckedForeColor = System.Drawing.Color.White;
+            this.btnIngreso.CheckedImageTint = System.Drawing.Color.White;
+            this.btnIngreso.CheckedOutline = System.Drawing.Color.DodgerBlue;
+            this.btnIngreso.CustomDialogResult = System.Windows.Forms.DialogResult.None;
+            this.btnIngreso.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnIngreso.HoverBackground = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(255)))), ((int)(((byte)(106)))), ((int)(((byte)(0)))));
+            this.btnIngreso.HoverForeColor = System.Drawing.Color.White;
+            this.btnIngreso.HoverImage = null;
+            this.btnIngreso.HoverImageTint = System.Drawing.Color.White;
+            this.btnIngreso.HoverOutline = System.Drawing.Color.Empty;
+            this.btnIngreso.Image = null;
+            this.btnIngreso.ImageAutoCenter = true;
+            this.btnIngreso.ImageExpand = new System.Drawing.Point(0, 0);
+            this.btnIngreso.ImageOffset = new System.Drawing.Point(0, 0);
+            this.btnIngreso.ImageTint = System.Drawing.Color.White;
+            this.btnIngreso.IsToggleButton = false;
+            this.btnIngreso.IsToggled = false;
+            this.btnIngreso.Location = new System.Drawing.Point(539, 310);
+            this.btnIngreso.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnIngreso.Name = "btnIngreso";
+            this.btnIngreso.NormalBackground = System.Drawing.Color.DodgerBlue;
+            this.btnIngreso.NormalForeColor = System.Drawing.Color.White;
+            this.btnIngreso.NormalOutline = System.Drawing.Color.Empty;
+            this.btnIngreso.OutlineThickness = 2F;
+            this.btnIngreso.PressedBackground = System.Drawing.Color.RoyalBlue;
+            this.btnIngreso.PressedForeColor = System.Drawing.Color.White;
+            this.btnIngreso.PressedImageTint = System.Drawing.Color.White;
+            this.btnIngreso.PressedOutline = System.Drawing.Color.Empty;
+            this.btnIngreso.Rounding = new System.Windows.Forms.Padding(5);
+            this.btnIngreso.Size = new System.Drawing.Size(204, 30);
+            this.btnIngreso.TabIndex = 10;
+            this.btnIngreso.TextAutoCenter = true;
+            this.btnIngreso.TextOffset = new System.Drawing.Point(0, 0);
+            this.btnIngreso.Click += new System.EventHandler(this.btnIngreso_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(577, 359);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(131, 13);
+            this.linkLabel1.TabIndex = 11;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "¿Olvidaste tu contraseña?";
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(582, 387);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(121, 13);
+            this.linkLabel2.TabIndex = 12;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "¿No tienes una cuenta?";
+            // 
+            // cbVer
+            // 
+            this.cbVer.AutoSize = true;
+            this.cbVer.Font = new System.Drawing.Font("Segoe UI Semibold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbVer.ForeColor = System.Drawing.Color.White;
+            this.cbVer.Location = new System.Drawing.Point(660, 266);
+            this.cbVer.Name = "cbVer";
+            this.cbVer.Size = new System.Drawing.Size(83, 30);
+            this.cbVer.TabIndex = 13;
+            this.cbVer.Text = "Mostrar\r\nContraseña\r\n";
+            this.cbVer.UseVisualStyleBackColor = true;
+            this.cbVer.CheckedChanged += new System.EventHandler(this.cbVer_CheckedChanged);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.ButtonText = "";
+            this.btnSalir.CheckedBackground = System.Drawing.Color.DarkOliveGreen;
+            this.btnSalir.CheckedForeColor = System.Drawing.Color.White;
+            this.btnSalir.CheckedImageTint = System.Drawing.Color.White;
+            this.btnSalir.CheckedOutline = System.Drawing.Color.DarkOliveGreen;
+            this.btnSalir.CustomDialogResult = System.Windows.Forms.DialogResult.None;
+            this.btnSalir.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnSalir.HoverBackground = System.Drawing.Color.FromArgb(((int)(((byte)(201)))), ((int)(((byte)(93)))), ((int)(((byte)(99)))));
+            this.btnSalir.HoverForeColor = System.Drawing.Color.White;
+            this.btnSalir.HoverImage = null;
+            this.btnSalir.HoverImageTint = System.Drawing.Color.White;
+            this.btnSalir.HoverOutline = System.Drawing.Color.Empty;
+            this.btnSalir.Image = global::Floristeria_SataUI.Properties.Resources.x;
+            this.btnSalir.ImageAutoCenter = true;
+            this.btnSalir.ImageExpand = new System.Drawing.Point(3, 3);
+            this.btnSalir.ImageOffset = new System.Drawing.Point(5, 0);
+            this.btnSalir.ImageTint = System.Drawing.Color.White;
+            this.btnSalir.IsToggleButton = false;
+            this.btnSalir.IsToggled = false;
+            this.btnSalir.Location = new System.Drawing.Point(760, 12);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.NormalBackground = System.Drawing.Color.DarkOliveGreen;
+            this.btnSalir.NormalForeColor = System.Drawing.Color.White;
+            this.btnSalir.NormalOutline = System.Drawing.Color.Empty;
+            this.btnSalir.OutlineThickness = 2F;
+            this.btnSalir.PressedBackground = System.Drawing.Color.FromArgb(((int)(((byte)(201)))), ((int)(((byte)(93)))), ((int)(((byte)(99)))));
+            this.btnSalir.PressedForeColor = System.Drawing.Color.White;
+            this.btnSalir.PressedImageTint = System.Drawing.Color.White;
+            this.btnSalir.PressedOutline = System.Drawing.Color.Empty;
+            this.btnSalir.Rounding = new System.Windows.Forms.Padding(5);
+            this.btnSalir.Size = new System.Drawing.Size(27, 25);
+            this.btnSalir.TabIndex = 14;
+            this.btnSalir.TextAutoCenter = true;
+            this.btnSalir.TextOffset = new System.Drawing.Point(0, 0);
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // sataPictureBox3
+            // 
+            this.sataPictureBox3.BorderCapStyle = System.Drawing.Drawing2D.DashCap.Flat;
+            this.sataPictureBox3.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(8)))), ((int)(((byte)(79)))), ((int)(((byte)(165)))));
+            this.sataPictureBox3.BorderColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(68)))), ((int)(((byte)(142)))));
+            this.sataPictureBox3.BorderLineStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+            this.sataPictureBox3.BorderSize = 0;
+            this.sataPictureBox3.GradientAngle = 50F;
+            this.sataPictureBox3.Location = new System.Drawing.Point(221, 464);
+            this.sataPictureBox3.Name = "sataPictureBox3";
+            this.sataPictureBox3.Size = new System.Drawing.Size(169, 169);
+            this.sataPictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.sataPictureBox3.TabIndex = 4;
+            this.sataPictureBox3.TabStop = false;
+            // 
+            // sataPictureBox2
+            // 
+            this.sataPictureBox2.BorderCapStyle = System.Drawing.Drawing2D.DashCap.Flat;
+            this.sataPictureBox2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(8)))), ((int)(((byte)(79)))), ((int)(((byte)(165)))));
+            this.sataPictureBox2.BorderColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(68)))), ((int)(((byte)(142)))));
+            this.sataPictureBox2.BorderLineStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+            this.sataPictureBox2.BorderSize = 0;
+            this.sataPictureBox2.GradientAngle = 50F;
+            this.sataPictureBox2.Location = new System.Drawing.Point(345, 336);
+            this.sataPictureBox2.Name = "sataPictureBox2";
+            this.sataPictureBox2.Size = new System.Drawing.Size(311, 311);
+            this.sataPictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.sataPictureBox2.TabIndex = 3;
+            this.sataPictureBox2.TabStop = false;
+            // 
+            // sataPictureBox1
+            // 
+            this.sataPictureBox1.BorderCapStyle = System.Drawing.Drawing2D.DashCap.Flat;
+            this.sataPictureBox1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(8)))), ((int)(((byte)(79)))), ((int)(((byte)(165)))));
+            this.sataPictureBox1.BorderColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(68)))), ((int)(((byte)(142)))));
+            this.sataPictureBox1.BorderLineStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+            this.sataPictureBox1.BorderSize = 0;
+            this.sataPictureBox1.GradientAngle = 50F;
+            this.sataPictureBox1.Location = new System.Drawing.Point(443, -136);
+            this.sataPictureBox1.Name = "sataPictureBox1";
+            this.sataPictureBox1.Size = new System.Drawing.Size(712, 712);
+            this.sataPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.sataPictureBox1.TabIndex = 2;
+            this.sataPictureBox1.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Floristeria_SataUI.Properties.Resources.LirioLogin;
+            this.pictureBox1.Location = new System.Drawing.Point(-180, -253);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(899, 943);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.ClientSize = new System.Drawing.Size(800, 517);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.cbVer);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.btnIngreso);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUs);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sataPictureBox3);
+            this.Controls.Add(this.sataPictureBox2);
+            this.Controls.Add(this.sataPictureBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "000000000000000000000000000000000000000000000000000000000000000000000000000000000" +
+    "00000000000000000000000000000000";
+            ((System.ComponentModel.ISupportInitialize)(this.sataPictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sataPictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sataPictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private SATAUiFramework.Controls.SATAEllipseControl sataEllipseControl1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private SATAUiFramework.Controls.SATAPictureBox sataPictureBox3;
+        private SATAUiFramework.Controls.SATAPictureBox sataPictureBox2;
+        private SATAUiFramework.Controls.SATAPictureBox sataPictureBox1;
+        private System.Windows.Forms.CheckBox cbVer;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private FrameworkTest.SATAButton btnIngreso;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.TextBox txtUs;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private FrameworkTest.SATAButton btnSalir;
+    }
+}
---
+++ Floristeria_SataUI/vistas/Productos.cs
@@ -0,0 +1,182 @@
+﻿using Floristeria_SataUI.models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Floristeria_SataUI.Controllers_query;
+
+namespace ProyectoFinal
+{
+    public partial class Productos : Form
+    {
+        public Productos()
+        {
+            InitializeComponent();
+            CargarProductos();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCrear_Click(object sender, EventArgs e)
+        {
+            
+
+          
+        }
+
+        private void CargarProductos()
+        {
+
+            btnCrear.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnCrear.Location = new Point(this.ClientSize.Width - btnCrear.Width - 20, 20);
+
+            Flow_panel.Location = new Point(20, 70);
+            Flow_panel.Size = new Size(this.ClientSize.Width - 40, this.ClientSize.Height - 100);
+            Flow_panel.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+
+
+            Flow_panel.Controls.Clear();
+            Flow_panel.AutoScroll = true;
+            Flow_panel.WrapContents = true;
+            Flow_panel.FlowDirection = FlowDirection.LeftToRight;
+          
+
+            Querys_produ querys = new Querys_produ();
+            List<product> productos = querys.get_all_products();
+
+            
+
+            foreach (var producto in productos)
+            {
+                Panel productPanel = new Panel
+                {
+                    Width = 200,
+                    Height = 300,
+                    BorderStyle = BorderStyle.FixedSingle,
+                    Margin = new Padding(10)
+                };
+               
+
+                PictureBox pictureBox = new PictureBox
+                {
+                    Width = 180,
+                    Height = 180,
+                    SizeMode = PictureBoxSizeMode.Zoom,
+                    Dock = DockStyle.Top
+                };
+                pictureBox.Click += (s, e) =>
+                {
+                    //details detail = new details(producto.Id);
+                   // detail.Show();
+                    
+                };
+
+                if (!string.IsNullOrEmpty(producto.Imagen) && File.Exists(producto.Imagen))
+                    pictureBox.ImageLocation = producto.Imagen;
+                else
+                    pictureBox.BackColor = Color.LightGray;
+
+                Label nameLabel = new Label
+                {
+                    Text = producto.Nombre,
+                    Dock = DockStyle.Top,
+                    TextAlign = ContentAlignment.MiddleCenter,
+                    Font = new Font("Arial", 10, FontStyle.Bold),
+                    Height = 30
+                };
+
+                Label id = new Label
+                {
+                    Dock = DockStyle.Top,
+                    TextAlign = ContentAlignment.MiddleCenter,
+                    Text = producto.Id.ToString(),
+                    
+                };
+
+                Label priceLabel = new Label
+                {
+                    Text = producto.Precio.ToString("C0", new CultureInfo("es-CO")),
+                    Dock = DockStyle.Top,
+                    TextAlign = ContentAlignment.MiddleCenter,
+                    Font = new Font("Arial", 10, FontStyle.Regular),
+                    Height = 30
+                };
+                Button btn_eliminar = new Button
+                {
+                    Text = "Eliminar",
+                    Dock = DockStyle.Bottom,
+                    Height = 30,
+                    BackColor = Color.LightCoral,
+                    ForeColor = Color.White
+                };
+
+                btn_eliminar.Click += (s, e) =>
+                {
+                    var result = MessageBox.Show($"¿Estás seguro de que deseas eliminar el producto '{producto.Nombre}'?", "Confirmar eliminación", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                    if (result == DialogResult.Yes)
+                    {
+                        querys.delete(producto.Id);
+                        CargarProductos();
+                    }
+                };
+
+                Button btn_actualizar = new Button
+                {
+                    Text = "Actualizar",
+                    Dock = DockStyle.Bottom,
+                    Height = 30,
+                    BackColor = Color.LightGreen,
+                    ForeColor = Color.White
+                };
+                btn_actualizar.Click += (sender, e) =>
+                {
+                   //actualizar actualizar = new actualizar(producto.Id);
+
+
+                    //actualizar.FormClosed += (s, args) =>
+                    {
+                        CargarProductos(); 
+                    };
+
+                    //actualizar.Show(); 
+
+
+
+                };
+
+
+
+
+                // Agregar en orden visible
+                productPanel.Controls.Add(priceLabel);
+                productPanel.Controls.Add(nameLabel);
+                productPanel.Controls.Add(pictureBox);
+                productPanel.Controls.Add(btn_actualizar);
+                productPanel.Controls.Add(btn_eliminar);
+               
+
+                Flow_panel.Controls.Add(productPanel);
+            }
+
+            Flow_panel.Refresh();
+            
+        }
+
+
+
+
+     
+
+    }
+}
